f9760c64780c4cc95f1d19033b1a73e46b58c65e
bug0tree <59710900+bug0tree@users.noreply.github.com>
2021-05-04 19:21:36 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+web3.eth.getBlock("latest", (error, result) => {
+  console.log('error:', error);
+  console.log('results', result);
+});
